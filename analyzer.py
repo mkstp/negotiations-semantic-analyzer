9acc11a7ce915f5ecdb8111b5dbe6ed24ab487fd
mkstp <marcstpierre93@gmail.com>
2024-12-18 22:14:52 -0800

    refactored response coverage function and removed print statements

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,8 @@
 from collections import defaultdict
 import torch
 
+#DEPRECATED
+
 #deals with tensor filtering
 def maskFilter(tensor, matchThreshold=0.45):
     #inputs a tensor and filters according to a threshold, then returns a list of indices
@@ -49,17 +51,16 @@ def tallySpeakerParam(parameterOutput, parameter, categories):
 
     return proportions
 
-#averages within individual speaker parameters
+
+# MAIN FUNCTIONS
+
+# calculates the proportion of responses of each person to the other person's statements
 def responseCoverage(df):
     # Group statements by 'name' and count unique 'id's for each name
     statement_counts = df.groupby('previous')['id'].nunique()
 
-    print(statement_counts)
-
     # Group responses by 'name' and count unique 'responseID's for each name
     response_counts = df.groupby('name')['responseID'].nunique()
-
-    print(response_counts)
 
     result = {}
 
@@ -75,8 +76,8 @@ def responseCoverage(df):
 
     return result
 
+# Calculating the metrics
 def computeMetrics(df):
-    # Calculating the metrics
     avgAirTime = df.groupby(['name', 'turn'])['airTime'].sum().groupby('name').mean()
     avgWPM = df.groupby('name')['wpm'].mean()
     avgResponse = df.groupby('name')['responseScore'].mean()
@@ -85,6 +86,7 @@ def computeMetrics(df):
     countNarrative = df.groupby(['name', 'nType'])['nType'].count()
     countEmotion = df.groupby(['name', 'emotion'])['emotion'].count()
     countTopic = df.groupby(['name', 'topic'])['topic'].count()
+    propCoverage = responseCoverage(df)
     
     # Initializing the dictionary
     result = {}
@@ -95,8 +97,9 @@ def computeMetrics(df):
         result[name] = {
             'avgAirTime': avgAirTime.get(name, None),
             'avgWPM': avgWPM.get(name, None),
-            'avgResponse': avgResponse.get(name, None),
-            'avgCoherence': avgCoherence.get(name, None),
+            'propResponseCover': propCoverage.get(name, None),
+            'avgResponseScore': avgResponse.get(name, None),
+            'avgCoherenceScore': avgCoherence.get(name, None),
             'countQuestions': countQuestions.loc[name].to_dict() if name in countQuestions else {},
             'countNarrative': countNarrative.loc[name].to_dict() if name in countNarrative else {},
             'countEmotion': countEmotion.loc[name].to_dict() if name in countEmotion else {},
