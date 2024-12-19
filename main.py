cc83a12e2fc5059f188e040ddb86ee013067f80f
mkstp <marcstpierre93@gmail.com>
2024-12-18 22:15:19 -0800

    streamline location string use, auto anonymize data enabled

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def analyzeTranscript(location='demoContent/demo_transcript.txt', topicString = " ", anonymizeFlag=False):
+def analyzeTranscript(location='demoContent/demo_transcript', topicString = " ", anonymizeFlag=False):
     import fathomPreprocessor
     import parameterizer
 
@@ -6,41 +6,42 @@ def analyzeTranscript(location='demoContent/demo_transcript.txt', topicString = 
     topics = [word.strip() for word in topics]
 
     # set flag to true to anonymize speakers, set to false to preserve names
+    location = f"meetingTranscripts/{location}.txt"
     raw = fathomPreprocessor.prepFile(location, anonymizeFlag)
     data = parameterizer.parameterize(raw[0], raw[1], raw[2], topics) # raw[0], raw[1], raw[2] = speakers, timespans, transcripts
 
     return data
 
-def outputJson(output, fileName = 'test.json'):
+def outputJson(output, fileName = 'test'):
     import json
+
+    fileName = f"meetingTranscripts/{fileName}.json"
 
     with open(fileName, "w") as json_file:
         json.dump(output, json_file, indent=4)
 
-def showReport(fileName = 'test.json', filter=None):
+def showReport(fileName = 'test', filter=None):
     import analyzer
     import visualizer
     import pandas as pd
 
+    fileName = f"meetingTranscripts/{fileName}.json"
+
     df = pd.read_json(fileName)
 
     metrics = analyzer.computeMetrics(df)
-    # print(analyzer.responseCoverage(df))
-    # print(metrics)
+    print(metrics)
 
     # Create and show plots
-
     visualizer.plotWPMAirTime(df, filter)
     visualizer.plotNarrativeEmotion(df, filter)
     visualizer.plotCoherenceResponsiveness(df, filter)
     visualizer.analyze_cluster_proportions_with_distances(df, filter)
     visualizer.plot_response_and_coherence_frequency(df, filter)
 
-
-location = 'meetingTranscripts/negotiation_roleplay/d_b_12-10-2024.txt'
-fileName = 'meetingTranscripts/negotiation_roleplay/d_b_12-10-2024.json'
+location = 'negotiation_roleplay/d_b_12-10-2024'
 topicString = 'algae, snails, elders, enzymes, economy, culture, Pfizer, alzheimer\'s, mother'
 
-output = analyzeTranscript(location, topicString, False)
-outputJson(output, fileName)
-showReport(fileName)
+output = analyzeTranscript(location, topicString, True)
+outputJson(output, location)
+showReport(location)
